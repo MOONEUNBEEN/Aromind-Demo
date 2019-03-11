3e135367b55be8d800bea5cff66a26fe1e53b16a
MOONEUNBEEN <e_bb@daum.net>
2019-03-11 15:40:40 +0900

    Aromind Demo Version

--- lavue/front/src/components/product/View.vue
+++ lavue/front/src/components/product/View.vue
@@ -5,26 +5,41 @@
             <hr>
             <br>
             <div class="row">
-                <div class="col-md-8 col-md-offset-2">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <img :src="'http://localhost:8000/' + product.image" alt="" style="width: 60%;" class="card-img-top rounded mx-auto d-block">
-                            <div class="form-group">
-                                <label for="">Name</label>
-                                <p>{{ product.name }}</p>
-                                
-                            </div>
-                            <div class="form-group">
-                                <label for="">Price</label>
-                                <p>{{ product.price }}</p>
-                            </div>
-                            <div class="form-group">
-                                <label for="">Description</label>
-                                <div>{{ product.description }}</div>
+                <div class="media align-items-center" style="margin-left: 17%;">
+                    <img :src="'http://localhost:8000/' + product.image" alt="" style="width: 40%;" class="align-self-center mr-3">
+                    <div class="media-body" style="margin-left: 13%;">
+                        <h2 class="mt-0">{{ product.name }}</h2>
+                        <h4>Price {{ product.price }}</h4>
+                        <h5>Made by {{ product.user_id }}</h5>
+                        <div class="input-group mb-3">
+                            <h5>Quantity</h5>
+                            <input type="number" class="form-control col-2" placeholder="1" aria-label="Recipient's username" aria-describedby="button-addon2" style="margin-left: 4%;">
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary1" type="button" id="button-addon2">개</button>
                             </div>
                         </div>
+                        <h5>Delivery 1~2day</h5>
+                        <button class="btn btn-outline-secondary1 btn-lg" style="width: 30%;">Wish List</button>
+                        <button class="btn btn-primary1 btn-lg" style="width: 30%;">To Order</button>
                     </div>
                 </div>
+            </div>
+            <br>
+            <div>
+                <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                    <div class="btn-group w-100" role="group" aria-label="First group">
+                        <button type="button" class="btn btn-primary1">상품 정보</button>
+                        <button type="button" class="btn btn-outline-secondary1">상품 리뷰</button>
+                        <button type="button" class="btn btn-outline-secondary1">Q&A</button>
+                        <button type="button" class="btn btn-outline-secondary1">판매자 정보</button>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div>
+                <h2 style="float: left; color: rgb(234,119,142);">▶</h2>
+                <h2>{{ product.name }}</h2>
+                <h5 style="margin-left: 3%;">{{ product.description }}</h5>
             </div>
         </div>
     </div>
